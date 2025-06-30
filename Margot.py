a70699f561c5468055cdf765c6fd05cad67c87ce
Margot Lafond <margot.lafond@etu.minesparis.psl.eu>
2025-06-30 11:16:20 +0200

    Donnees sur la pollution et le NO2

---
+++ Margot.py
@@ -0,0 +1,15 @@
+# Le NO2 est nocif pour la santé à court et long terme et responsable d’un grand nombre de décès. Le seuil recommandé par l'OMS est dépassé partout en Île de France, il est émis par les véhicules essence et diesel. $\newline$
+# Quand il réagit avec l’ammoniac, il forme du nitrate d’ammonium qui représente 1/3 des particules fines d’Île de France sur 1 an.
+#
+# Seuils : $\newline$
+# PM2,5 : $\newline$
+#     • sur 1 an : 5 µg/m³ $\newline$
+#     • 24h : 15 µg/m³ $\newline$
+# PM10 : $\newline$
+#     • sur 1 an : 15 µg/m³ $\newline$
+#     • 24h : 45 µg/m³ $\newline$
+# NO2 : $\newline$
+#     • sur 1 an : 10 µg/m³ $\newline$
+#     • 24h : 25 µg/m³
+
+
